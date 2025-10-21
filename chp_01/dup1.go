e1daa9d748fce19c5daa05c14aeb9f0d8ff4e7ea
Godswill Umukoro <umukoro6@gmail.com>
2025-10-21 05:52:03 +0100

    update to read the entire input into memory, instead of streaming

--- chp_01/dup1.go
+++ chp_01/dup1.go
@@ -1,38 +1,27 @@
-// Prints the count and text that appear more than once, coming from stdin or a list of named files
+// Prints the count and text that appear more than once, coming from stdin or a list of named files by reading the entire input into memory, instead of streaming mode.
 package main
 
 import (
 	"fmt"
-	"bufio"
 	"os"
+	"strings"
 )
 
 func main() {
 	counts := make(map[string]int)
-	files := os.Args[1:]
-	if len(files) == 0 {
-		countLines(os.Stdin, counts)
-	} else {
-		for _, arg := range files {
-			f, err := os.Open(arg)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "error: %v\n", err)
-				continue
-			}
-			countLines(f, counts)
-			f.Close()
+	for _, f := range os.Args[1:] {
+		data, err := os.ReadFile(f)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error: %v\n", err)
+			continue
+		}
+		for _, line := range strings.Split(string(data), "\n") {
+			counts[line]++
 		}
 	}
 	for line, n := range counts {
 		if n > 1 {
 			fmt.Printf("%d\t%s\n", n, line)
 		}
-	}
-}
-
-func countLines(f *os.File, counts map[string]int) {
-	input := bufio.NewScanner(f)
-	for input.Scan() {
-		counts[input.Text()]++
 	}
 }
